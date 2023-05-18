7a649dc0466218b2b0c8a9f8d4dbd6db124bec28
1danielsc <danielscolao@gmail.com>
2023-05-18 16:30:53 -0300

    virtual threads

--- src/main/java/com/review/ReviewBackApplication.java
+++ src/main/java/com/review/ReviewBackApplication.java
@@ -1,7 +1,10 @@
 package com.review;
 
+import java.util.concurrent.Executors;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -28,5 +31,11 @@ public class ReviewBackApplication {
 	// @Bean
 	// public RestTemplate getRestTemplate(){
 	// 	return new RestTemplate();
+	// }
+
+	/* UNCOMMENT TO ENABLE VIRTUAL THREADS */
+	// @Bean
+	// public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+	// 	return protocolHandler -> protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
 	// }
 }
